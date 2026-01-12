ad75619e02504c617b27614d8cab08fe0926cb1a
gamelxrd <mzp.simson.sav@gmail.com>
2026-01-12 04:37:43 +0300

    -

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -194,10 +194,10 @@ const LandingPage: React.FC<LandingPageProps> = ({ onOpenCalculator }) => {
         {/* Calculator Button (Special) */}
         <button
           onClick={onOpenCalculator}
-          className="w-full group bg-gradient-to-r from-purple-900/60 to-blue-900/60 hover:from-purple-800 hover:to-blue-800 backdrop-blur-xl border border-white/20 p-4 md:p-5 rounded-2xl flex items-center justify-between transition-all duration-300 hover:scale-[1.02] shadow-xl mb-8"
+          className="w-full group bg-gradient-to-r from-purple-900/20 to-blue-900/20 hover:from-purple-800/50 hover:to-blue-800/50 backdrop-blur-xl border border-white/20 p-4 md:p-5 rounded-2xl flex items-center justify-between transition-all duration-300 hover:scale-[1.02] shadow-xl mb-8"
         >
             <div className="flex items-center gap-5">
-                <div className="p-3 bg-white/10 rounded-2xl text-white">
+                <div className="p-3 rounded-2xl text-white">
                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v12m-3-2.818.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                    </svg>
