fc09786d96c60ab21c541dd097dbc39f31ef4ccc
gamelxrd <mzp.simson.sav@gmail.com>
2026-01-24 22:33:04 +0300

    -

--- services/userService.ts
+++ services/userService.ts
@@ -207,6 +207,7 @@ const WATCHED_DB: Record<string, number> = {
   "tt7631058": 5,   // The Lord of the Rings: The Rings of Power 
   "tt6820256": 7,   // Arctic
   "tt1601913": 6,   // The Grey
+  "tt0944834": 8,   // Caçadores de Dragões
 };
 
 export const getUserRating = async (imdbID: string): Promise<number | undefined> => {
